69a25ab703614e7ca733bfcbb93b84dff44ce150
its.kenaye <144159042+itsKenaye@users.noreply.github.com>
2024-12-04 18:20:04 -0600

    Update summarize_tree.c

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -5,9 +5,9 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
-
+//newly added
+#include <errno.h>
 static int num_dirs, num_regular;
-
 bool is_dir(const char* path) {
   /*
    * Use the stat() function (try "man 2 stat") to determine if the file
@@ -16,14 +16,19 @@ bool is_dir(const char* path) {
    * return value from stat() in case there is a problem, e.g., maybe the
    * the file doesn't actually exist.
    */
+  struct stat buf;
+int stat_error = stat(path, &buf);
+if(stat_error == 0) {
+return S_ISDIR(buf.st_mode);
 }
-
+fprintf(stderr, "Error: the path is not a directory.\n");
+exit(1);
+}
 /* 
  * I needed this because the multiple recursion means there's no way to
  * order them so that the definitions all precede the cause.
  */
 void process_path(const char*);
-
 void process_directory(const char* path) {
   /*
    * Update the number of directories seen, use opendir() to open the
@@ -36,15 +41,35 @@ void process_directory(const char* path) {
    * with a matching call to chdir() to move back out of it when you're
    * done.
    */
+  num_dirs++;
+int chdir_err = chdir(path);
+if(chdir_err != 0){
+perror("Error: change directory failed\n");
+fprintf(stderr, "Path: %s\n", path);
+exit(1);
 }
-
+DIR *dir = opendir(".");
+struct dirent *ent;
+while((ent = readdir(dir)) != NULL){
+if(strcmp(ent->d_name, "..") == 0 || strcmp(ent->d_name, ".") == 0){
+continue;
+}
+process_path(ent->d_name);
+}
+closedir(dir);
+int second_chdir_err = chdir("..");
+if(second_chdir_err != 0){
+fprintf(stderr, "Path: %s\n", path);
+exit(1);
+}
+}
 void process_file(const char* path) {
   /*
    * Update the number of regular files.
    * This is as simple as it seems. :-)
    */
+  num_regular++;
 }
-
 void process_path(const char* path) {
   if (is_dir(path)) {
     process_directory(path);
@@ -52,7 +77,6 @@ void process_path(const char* path) {
     process_file(path);
   }
 }
-
 int main (int argc, char *argv[]) {
   // Ensure an argument was provided.
   if (argc != 2) {
@@ -60,14 +84,12 @@ int main (int argc, char *argv[]) {
     printf ("       where <path> is the file or root of the tree you want to summarize.\n");
     return 1;
   }
-
   num_dirs = 0;
   num_regular = 0;
-
   process_path(argv[1]);
-
   printf("There were %d directories.\n", num_dirs);
   printf("There were %d regular files.\n", num_regular);
-
   return 0;
 }
+ 
+
