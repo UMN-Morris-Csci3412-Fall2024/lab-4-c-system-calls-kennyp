ffb5874d4df0ccd93f7983f17753f1759648efd6
its.kenaye <144159042+itsKenaye@users.noreply.github.com>
2024-12-04 18:21:20 -0600

    Create summarize_tree_ftw.c

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <ftw.h>
+#define MAX_FTW_DEPTH 16
+//declaim
+static int num_dirs, num_regular;
+static int callback(const char *, const struct stat *, int);
+//main
+int main(int argc, char** argv) {
+if(argc != 2) {
+printf("Usage: %s <path>\n", argv[0]);
+printf("where <path> is the file or root of the tree you want to summarize.\n");
+exit(1);
+}
+num_dirs = 0;
+num_regular = 0;
+//here is the difference when comapred with summarize_tree.c
+ftw(argv[1], callback, MAX_FTW_DEPTH);
+printf("There were %d directories.\n", num_dirs);
+printf("There were %d regular files.\n", num_regular);
+}
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+if(S_ISDIR(sb->st_mode)) {
+num_dirs++;
+}
+else {
+num_regular++;
+}
+return 0;
+}
+ 
+
